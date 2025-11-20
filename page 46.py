3b8e25c5b0917c05367bfd87025209a6f1ee7660
tchiofouod-jpg <tchiofouod@gmail.com>
2025-11-20 16:02:45 +0100

    Add files via upload

---
+++ page 46.py
@@ -0,0 +1,22 @@
+# 3-8seeing the word
+travel_list=['Egypt','Cape town','algeria','Nigeria',]
+print(travel_list)
+travel_list.sort()
+print(travel_list)
+
+travel_list.sort(reverse=True)
+print(travel_list)
+
+travel_list.reverse()
+print(travel_list)
+
+travel_list.reverse()
+print(travel_list)
+
+travel_list.sort()
+print(travel_list)
+
+travel_list.sort(reverse=True)
+print(travel_list)    
+             
+            
---
+++ page43.py
@@ -0,0 +1,41 @@
+Guest_list('DOA', 'CEDRIC', 'MATHEW', 'BORIS', 'DIANE')
+Print('Guest_list'[0], 'you are invited to my deaner party')
+print('Guest_list'[1], 'you are invited to my deaner party')
+print('Guest_list'[2], 'you are invited to my deaner party')
+print('Guest_list'[3], 'you are invited to my deaner party')
+print('Guest_list'[4], 'you are invited to my deaner party')
+
+#3-5 changing the guest list
+Guest_list('DOA', 'CEDRIC', 'MATHEW', 'BORIS', 'DIANE')
+Print('Guest_list'[0], 'you are invited to my deaner party')
+print('Guest_list'[1], 'you are invited to my deaner party')
+print('Guest_list'[2], 'you are invited to my deaner party')
+print('Guest_list'[3], ',you are invited to my deaner party')
+print('Guest_list'[4], 'you are invited to my deaner party')
+
+print('Guest_list'[0], 'Dear guest sorry your invitation hasbein declined')
+
+Guest_list.pop[0]
+print(Guest_list)
+Guest_list.insert(0,'stercy')
+print(Guest_list)
+Print('Guest_list'[0], 'you are invited to my deaner party')
+print('Guest_list'[1], 'you are invited to my deaner party')
+print('Guest_list'[2], 'you are invited to my deaner party')
+print('Guest_list'[3], ',you are invited to my deaner party')
+print('Guest_list'[4], 'you are invited to my deaner party')
+
+print('dear Guest we have found a bigger table so we decided to add 3 more guest')
+
+Guest_list.insert(0, 'brain')
+Guest_list.insert(4, 'bless')
+Guest_list.append('piresse')
+
+
+Print('Guest_list'[0], 'you are invited to my deaner party')
+print('Guest_list'[1], 'you are invited to my deaner party')
+print('Guest_list'[2], 'you are invited to my deaner party')
+print('Guest_list'[3], ',you are invited to my deaner party')
+print('Guest_list'[4], 'you are invited to my deaner party')
+print('Guest_list'[5], ',you are invited to my deaner party')
+print('Guest_list'[6], 'you are invited to my deaner party')
